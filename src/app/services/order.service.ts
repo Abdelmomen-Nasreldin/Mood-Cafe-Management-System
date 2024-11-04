70dbf1e314218b373dcedb0c986aae2e4bbb89dc
Abdelmomen Nasreldin <abdelmomennasreldin@gmail.com>
2024-11-05 00:15:22 +0200

    order sidebar

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { subDays, isWithinInterval } from 'date-fns'; // Use date-fns for easy date handling
-import { IOrder } from '../models/order';
+import { calculateItemTotal, IOrder, IOrderItem } from '../models/order';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -13,11 +13,38 @@ export class OrderService {
   public get enableOrdering() {
     return this._enableOrdering.asObservable();
   }
-  public setEnableOrdering(value : boolean) {
+  public setEnableOrdering(value: boolean) {
     this._enableOrdering.next(value);
   }
 
-  getOrders() : IOrder[] {
+  private _orderedSidebarItems = new BehaviorSubject<IOrderItem[]>([]);
+
+  public getOrderedSidebarItems() {
+    return this._orderedSidebarItems.asObservable();
+  }
+  public addOrderedSidebarItems(orderItem: IOrderItem) {
+    const currentOrders = this._orderedSidebarItems.getValue();
+    const itemIndex = currentOrders.findIndex(
+      (ele) => ele.itemName === orderItem.itemName
+    );
+    if (itemIndex == -1) {
+      currentOrders.push(orderItem);
+    } else {
+      currentOrders[itemIndex].quantity++;
+      currentOrders[itemIndex].total = calculateItemTotal(
+        currentOrders[itemIndex]
+      );
+    }
+    this._orderedSidebarItems.next(currentOrders);
+  }
+
+  public resetOrderedSidebarItems(){
+    this._orderedSidebarItems.next([]);
+  }
+
+  // orders
+
+  getOrders(): IOrder[] {
     const orders = localStorage.getItem(this.ordersKey);
     return orders ? JSON.parse(orders) : [];
   }
@@ -36,20 +63,21 @@ export class OrderService {
 
   updateOrder(updatedOrder: IOrder) {
     let orders = this.getOrders();
-    const index = orders.findIndex((order: IOrder) => order.orderId === updatedOrder.orderId);
+    const index = orders.findIndex(
+      (order: IOrder) => order.orderId === updatedOrder.orderId
+    );
     if (index > -1) {
       orders[index] = updatedOrder;
       localStorage.setItem(this.ordersKey, JSON.stringify(orders));
     }
   }
 
-
   // tracking orders
   getWeeklyOrders() {
     const today = new Date();
     const weekAgo = subDays(today, 7);
     const orders = this.getOrders();
-    return orders.filter(order =>
+    return orders.filter((order) =>
       isWithinInterval(new Date(order.date), { start: weekAgo, end: today })
     );
   }
@@ -58,7 +86,7 @@ export class OrderService {
     const today = new Date();
     const monthAgo = subDays(today, 30);
     const orders = this.getOrders();
-    return orders.filter(order =>
+    return orders.filter((order) =>
       isWithinInterval(new Date(order.date), { start: monthAgo, end: today })
     );
   }
