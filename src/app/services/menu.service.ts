dd632e971cd5f0745cbb3544528fdd4d3815f93e
Abdelmomen Nasreldin <abdelmomennasreldin@gmail.com>
2024-11-06 13:48:23 +0200

    rtl lang

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -10,9 +10,12 @@ export class MenuService {
 
   getMenuItems() : IMenuItem[] {
     return [
-      { id: 1, name: 'Latte', price: 30 },
-      { id: 2, name: 'Espresso', price: 25 },
-      { id: 3, name: 'Cappuccino', price: 35 },
+      { id: 1, name: 'لاتيه', price: 30 },
+      { id: 2, name: 'اسبرسو', price: 25 },
+      { id: 3, name: 'كابتشينو', price: 35 },
+      { id: 4, name: 'شاي', price: 9 },
+      { id: 5, name: 'قهوة', price: 18 },
+      { id: 6, name: ' قهوة فرنساوي', price: 25 },
     ];
   }
 }
