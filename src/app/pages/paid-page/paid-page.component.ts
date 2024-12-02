50a09be613f1dfcc6db110233bfd60e190dfb2d8
hadywafa <97hadywafa@gmail.com>
2024-12-02 19:31:38 +0200

    create new pages paid, postponed and cancelled

---
+++ src/app/pages/paid-page/paid-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paid-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './paid-page.component.html',
+  styleUrl: './paid-page.component.scss'
+})
+export class PaidPageComponent {
+
+}
---
+++ src/app/pages/postponed-page/postponed-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postponed-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './postponed-page.component.html',
+  styleUrl: './postponed-page.component.scss'
+})
+export class PostponedPageComponent {
+
+}
